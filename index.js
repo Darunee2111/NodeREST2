8b9bfcf7662d1e9561bc1a61311a0afcbccf84fe
Darunee2111 <s6406021610011@email.kmutnb.ac.th>
2024-02-06 21:26:30 +0700

    save

--- index.js
+++ index.js
@@ -70,5 +70,5 @@ app.delete('/books/:id', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port,() => console.log(`Example app listening at http://localhost:${port}`));
